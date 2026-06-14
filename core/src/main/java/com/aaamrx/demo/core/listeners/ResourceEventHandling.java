50bb292c792dc2c95d7dfcbb5a7813bf8a8ffdd2
Sriram G A <1998.aaamr.x@gmail.com>
2026-06-14 13:20:07 +0530

    Changes in Test Branch

--- core/src/main/java/com/aaamrx/demo/core/listeners/ResourceEventHandling.java
+++ core/src/main/java/com/aaamrx/demo/core/listeners/ResourceEventHandling.java
@@ -48,6 +48,6 @@ public class ResourceEventHandling implements ResourceChangeListener {
             
         }
     }
-    //Test to check if the changes from different branch is visible in VS code form GIT
+    //Test to check if the changes from VS code is visible in testBranch to GIT.
     
 }
