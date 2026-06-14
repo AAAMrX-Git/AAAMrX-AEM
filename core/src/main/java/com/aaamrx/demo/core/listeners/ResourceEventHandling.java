5f8126b96c523e84e1baa8cc39f68a93180cd434
AAAMrX-Git <1998.aaamr.x@gmail.com>
2026-06-14 13:11:21 +0530

    Update ResourceEventHandling.java

--- core/src/main/java/com/aaamrx/demo/core/listeners/ResourceEventHandling.java
+++ core/src/main/java/com/aaamrx/demo/core/listeners/ResourceEventHandling.java
@@ -48,6 +48,6 @@ public class ResourceEventHandling implements ResourceChangeListener {
             
         }
     }
-    //Test to check if the changes are visible in GIT.
+    //Test to check if the changes are visible in VS code to GIT.
     
 }
